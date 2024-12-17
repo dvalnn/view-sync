83a6afc6a3a55b853d0a5ea9f073b6740932198b
dvalinn <tiago.andre.amorim@gmail.com>
2024-12-17 22:02:09 +0000

    heartbeat msg serialize/deserialize

--- src/cbcast/message.c
+++ src/cbcast/message.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+static char *cbc_msg_serialize_heartbeat(const cbcast_msg_t *msg,
+                                         size_t *out_size);
+
+static char *cbc_msg_serialize_data(const cbcast_msg_t *msg, size_t *out_size);
+
 Result *cbc_msg_create_header(cbcast_msg_kind_t kind, uint16_t len) {
   cbcast_msg_hdr_t *hdr = calloc(1, sizeof(cbcast_msg_hdr_t));
   if (!hdr) {
@@ -22,13 +27,19 @@ Result *cbc_msg_create(cbcast_msg_hdr_t *header, char *payload) {
   if (!message) {
     return result_new_err("[cbc_create_message] Alloc failed");
   }
+
+  message->header = header;
+  if (header->kind == CBC_HEARTBEAT) {
+    message->header->len = 0;
+    return result_new_ok(message);
+  }
+
   message->payload = calloc(strlen(payload) + 1, sizeof(char));
   if (!message->payload) {
     return result_new_err("[cbc_create_message] Payload alloc failed");
   }
 
   memcpy(message->payload, payload, strlen(payload) + 1);
-  message->header = header;
 
   return result_new_ok(message);
 }
@@ -47,33 +58,21 @@ void cbc_msg_free(cbcast_msg_t *msg) {
 }
 
 char *cbc_msg_serialize(const cbcast_msg_t *msg, size_t *out_size) {
-  if (!msg || !msg->header || !msg->payload) {
-    fprintf(stderr, "[cbc_msg_serialize] Invalid message structure\n");
+  if (!msg || !msg->header) {
+    fprintf(stderr, "[cbc_msg_serialize] Invalid message or header\n");
     return NULL;
   }
 
-  // Calculate total size of the serialized message
-  size_t total_size = sizeof(cbcast_msg_hdr_t) + msg->header->len +
-                      1; // Include null terminator
-  char *serialized = calloc(total_size, sizeof(char));
-  if (!serialized) {
-    fprintf(stderr, "[cbc_msg_serialize] Memory allocation failed\n");
-    return NULL;
+  switch (msg->header->kind) {
+
+  case CBC_HEARTBEAT:
+    return cbc_msg_serialize_heartbeat(msg, out_size);
+  case CBC_RETRANSMIT:
+  case CBC_DATA:
+    return cbc_msg_serialize_data(msg, out_size);
   }
 
-  // Copy the header and payload into the serialized buffer
-  memcpy(serialized, msg->header, sizeof(cbcast_msg_hdr_t));
-  memcpy(serialized + sizeof(cbcast_msg_hdr_t), msg->payload,
-         msg->header->len + 1);
-
-  // Debug output
-  printf("[cbc_msg_serialize] Serialized message: total_size=%zu, "
-         "header_size=%zu, payload_size=%d\n",
-         total_size, sizeof(cbcast_msg_hdr_t), msg->header->len + 1);
-
-  *out_size = total_size;
-
-  return serialized;
+  return result_unwrap(result_new_err("UNREACHABLE"));
 }
 
 Result *cbc_msg_deserialize(const char *bytes) {
@@ -96,6 +95,15 @@ Result *cbc_msg_deserialize(const char *bytes) {
         "[cbc_msg_deserialize] Memory allocation failed for header");
   }
   memcpy(msg->header, bytes, sizeof(cbcast_msg_hdr_t));
+
+  if (msg->header->kind == CBC_HEARTBEAT) {
+    // Debug output
+    printf(
+        "[cbc_msg_deserialize] Deserialized heartbeat msg message: clock=%d\n",
+        msg->header->clock);
+
+    return result_new_ok(msg); // hearbeat has no payload
+  }
 
   // Validate header length to prevent overflows
   if (msg->header->len == 0) {
@@ -122,4 +130,47 @@ Result *cbc_msg_deserialize(const char *bytes) {
          msg->header->kind, msg->header->clock, msg->header->len, msg->payload);
 
   return result_new_ok(msg);
+}
+
+static char *cbc_msg_serialize_heartbeat(const cbcast_msg_t *msg,
+                                         size_t *out_size) {
+  size_t total_size = sizeof(cbcast_msg_hdr_t);
+  char *serialized = calloc(total_size, sizeof(char));
+  if (!serialized) {
+    fprintf(stderr, "[cbc_msg_serialize_heartbeat] Memory allocation failed\n");
+    return NULL;
+  }
+
+  memcpy(serialized, msg->header, sizeof(cbcast_msg_hdr_t));
+  *out_size = total_size;
+  return serialized;
+}
+
+static char *cbc_msg_serialize_data(const cbcast_msg_t *msg, size_t *out_size) {
+  if (!msg->header->len || !msg->payload) {
+    fprintf(stderr, "[cbc_msg_serialize_data] Invalid message\n");
+    return NULL;
+  }
+
+  // Calculate total size of the serialized message
+  size_t total_size = sizeof(cbcast_msg_hdr_t) + msg->header->len +
+                      1; // Include null terminator
+  char *serialized = calloc(total_size, sizeof(char));
+  if (!serialized) {
+    fprintf(stderr, "[cbc_msg_serialize_data] Memory allocation failed\n");
+    return NULL;
+  }
+
+  // Copy the header and payload into the serialized buffer
+  memcpy(serialized, msg->header, sizeof(cbcast_msg_hdr_t));
+  memcpy(serialized + sizeof(cbcast_msg_hdr_t), msg->payload,
+         msg->header->len + 1);
+
+  // Debug output
+  printf("[cbc_msg_serialize_data] Serialized message: total_size=%zu, "
+         "header_size=%zu, payload_size=%d\n",
+         total_size, sizeof(cbcast_msg_hdr_t), msg->header->len + 1);
+
+  *out_size = total_size;
+  return serialized;
 }
